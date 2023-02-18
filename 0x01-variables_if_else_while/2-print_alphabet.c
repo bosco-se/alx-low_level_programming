cdb8f015c2c9df2b757b16f84997394cc5550300
bosco-se <donbosco36.db@outlook.com>
2023-02-17 17:41:01 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print alpha in lower case
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(ls);
+	}
+	putchar('\n');
+	return (0);
+}
