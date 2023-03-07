b40faa2d89762f6077a3bb6059160327ca33907e
bosco-se <donbosco36.db@outlook.com>
2023-03-06 17:42:14 -0800

    2&3

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  *_memcpy - a function that copies memory area
  *@dest: memory where is stored
@@ -9,12 +10,13 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-        int r = 0;
-        int i = n;
-        for (; r < i; r++)
-        {
-                dest[r] = src[r];
-                n--;
-        }
-        return (dest);
+	int r = 0;
+	int i = n;
+
+	for (; r < i; r++)
+	{
+		dest[r] = src[r];
+		n--;
+	}
+	return (dest);
 }
--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strspn - Entry point
  * @s: input
@@ -7,21 +8,22 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-        unsigned int n = 0;
-        int r;
-        while (*s)
-        {
-                for (r = 0; accept[r]; r++)
-                {
-                        if (*s == accept[r])
-                        {
-                                n++;
-                                break;
-                        }
-                        else if (accept[r + 1] == '\0')
-                                return (n);
-                }
-                s++;
-        }
-        return (n);
+	unsigned int n = 0;
+	int r;
+	
+	while (*s)
+	{
+		for (r = 0; accept[r]; r++)
+		{
+			if (*s == accept[r])
+			{
+				n++;
+				break;
+			}
+			else if (accept[r + 1] == '\0')
+				return (n);
+		}
+		s++;
+	}
+	return (n);
 }
