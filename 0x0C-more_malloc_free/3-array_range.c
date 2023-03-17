af18d6488e48fa7a1e388aeb9680b5b81ec85b39
bosco-se <donbosco36.db@outlook.com>
2023-03-16 19:36:20 -0700

    corrections

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -4,7 +4,6 @@
  * array_range - creates an array of integers.
  * @min: minimum value.
  * @max: maximum value.
- * 
  * Return: pointer to the newly created array.
  * if man > mix, returns NULL.
  * if malloc fails, returns NULL.
